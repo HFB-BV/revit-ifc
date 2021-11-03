2ea5df684ebd4aea46fed864e392253f0afd0f4d
nverhaegen-hfb <n.verhaegen@hfb-groep.nl>
2021-11-03 09:52:49 +0100

    Added family and IfcExportAs logging

--- Source/Revit.IFC.Export/Exporter/Exporter.cs
+++ Source/Revit.IFC.Export/Exporter/Exporter.cs
@@ -167,7 +167,7 @@ namespace Revit.IFC.Export.Exporter
             logFileName = "NoTitle_" + DateTime.Now.ToString("yyyy-MM-dd_HHmmss") + ".log";
          }
 
-         hfblogger = new HFBExportLogger(@"N:\Revit\IFC Exports\IFClogs\2021", logFileName);
+         hfblogger = new HFBExportLogger(document, @"N:\Revit\IFC Exports\IFClogs\2021", logFileName);
 
          try
          {
