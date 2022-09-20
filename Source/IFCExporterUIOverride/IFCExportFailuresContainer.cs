77ad56169272c557c139899758b432ca632e6145
nverhaegen-hfb <n.verhaegen@hfb-groep.nl>
2022-09-20 10:05:12 +0200

    Split up ExportLinkedDocuments into a part with the UI logic and a part with the business logic. Also added a static version that can be called from scripts

---
+++ Source/IFCExporterUIOverride/IFCExportFailuresContainer.cs
@@ -0,0 +1,30 @@
+﻿using System.Collections.Generic;
+using Autodesk.Revit.DB;
+
+namespace BIM.IFC.Export.UI
+{
+   class IFCExportFailuresContainer
+   {
+      // Reasons we can't export:
+      // 1. The path for the linked instance doesn't exist.
+      // 2. Couldn't create a temporary document for exporting the linked instance.
+      // 3. The document for the linked instance can't be found.
+      // 4. The linked instance is mirrored, non-conformal, or scaled.
+
+      public IList<string> pathDoesntExist = new List<string>();
+      public IList<string> noTempDoc = new List<string>();
+      public IList<ElementId> cantFindDoc = new List<ElementId>();
+      public IList<ElementId> nonConformalInst = new List<ElementId>();
+      public IList<ElementId> scaledInst = new List<ElementId>();
+      public IList<ElementId> instHasReflection = new List<ElementId>();
+
+      public IFCExportFailuresContainer() { }
+
+      public int GetNumBadInstances()
+      {
+         return pathDoesntExist.Count + noTempDoc.Count + cantFindDoc.Count + nonConformalInst.Count
+       + scaledInst.Count + instHasReflection.Count;
+      }
+
+   }
+}
